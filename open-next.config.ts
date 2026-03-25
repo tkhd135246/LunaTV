c46d232f0f36246f66f17a5c9958ab2ccc61d815
tkhd135246 <dennyhokt@gmail.com>
2026-03-25 19:54:21 +0800

    fix open-next config esm import

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,13 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+// open-next.config.ts
+
 // import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-	// For best results consider enabling R2 caching
-	// See https://opennext.js.org/cloudflare/caching for more details
-	// incrementalCache: r2IncrementalCache
-});
+export default async () => {
+  const { defineCloudflareConfig } = await import("@opennextjs/cloudflare");
+
+  return defineCloudflareConfig({
+    // For best results consider enabling R2 caching
+    // See https://opennext.js.org/cloudflare/caching for more details
+    // incrementalCache: r2IncrementalCache(),
+  });
+};
